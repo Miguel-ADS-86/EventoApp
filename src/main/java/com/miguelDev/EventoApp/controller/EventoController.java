a5214c99c0c50a948667cfd6aea9639fd0b87f2f
Miguel-ADS-86 <miguelSistenas.emp@gmail.com>
2023-08-02 13:38:06 -0300

    implementação do eventoController, funcionalidade de cadastro evento, criação da pagina cadastroEvento com seu formulario simples

--- src/main/java/com/miguelDev/EventoApp/controller/EventoController.java
+++ src/main/java/com/miguelDev/EventoApp/controller/EventoController.java
@@ -1,9 +1,26 @@
 package com.miguelDev.EventoApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.miguelDev.EventoApp.model.Evento;
+import com.miguelDev.EventoApp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
-    
+    @Autowired
+	private EventoRepository eventoRepository;
 	
+	@RequestMapping(value="/cadastro", method = RequestMethod.GET)
+	public String getEvento() {
+		return "paginas/cadastroEvento";
+	}
+	
+	@RequestMapping(value="cadastro", method = RequestMethod.POST)
+	public String setEvento(Evento evento) {
+	 	eventoRepository.save(evento);
+	 	return "redirect:cadastro";
+	}
 }
